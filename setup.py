bc3fa846b0ee201c144099eaf03d02062ca46c30
Yukino Ikegami <yknikgm@gmail.com>
2019-02-05 20:14:49 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from codecs import open
+import os
+import re
+from setuptools import setup
+
+with open(os.path.join('yasklearn', '__init__.py'), 'r', encoding='utf8') as f:
+    version = re.compile(
+        r".*__version__ = '(.*?)'", re.S).match(f.read()).group(1)
+
+setup(
+    name='yascikit-learn',
+    packages=['yasklearn'],
+    version=version,
+    license='MIT License',
+    description='Yet another scikit-learn',
+    author='Yukino Ikegami',
+    author_email='yknikgm@gmail.com',
+    url='https://github.com/ikegami-yukino/yascikit-learn',
+    keywords=['Machine Learning'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        ],
+    data_files=[('', ['README.rst'])],
+    long_description='%s' % (open('README.md', encoding='utf8').read()),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas',
+        'scikit-learn',
+        'flati'
+    ]
+)
