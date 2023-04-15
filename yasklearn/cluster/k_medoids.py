9e6c544a883bf4f36cc5c02146b8baf72e7ca7d8
Yukino Ikegami <yknikgm@gmail.com>
2023-04-15 13:36:11 +0900

    Add predict, fit_predict and fit_transform methods in KMedoids

--- yasklearn/cluster/k_medoids.py
+++ yasklearn/cluster/k_medoids.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from scipy.spatial.distance import pdist, squareform
+from sklearn.cluster import KMeans
 from sklearn.utils import check_random_state
 
 
@@ -10,8 +11,10 @@ def _make_initial_medoids(distances, n_clusters, random_state):
     m, n = distances.shape
 
     distances_df = pd.DataFrame({'id': range(m)})
-    distances_df = pd.concat([distances_df, pd.DataFrame(distances,
-                                                         columns=[i for i in range(n)])], axis=1)
+    distances_df = pd.concat(
+        [distances_df,
+         pd.DataFrame(distances, columns=[i for i in range(n)])],
+        axis=1)
 
     medoids = []
     for cluster_num in range(n_clusters):
@@ -21,12 +24,16 @@ def _make_initial_medoids(distances, n_clusters, random_state):
             medoids.extend(medoid)
         else:
             distance = distances_df.drop(medoids, axis=0)
-            distance = distance.ix[:, ['id'] + medoids]
+            distance = distance.loc[:, ['id'] + medoids]
             distance['min_distance'] = distance.min(axis=1)
-            distance['min_distance_squared'] = distance['min_distance'] * distance['min_distance']
+            distance['min_distance_squared'] = distance[
+                'min_distance'] * distance['min_distance']
             ids = distance['id'].values
-            distance_values = distance['min_distance_squared'] / np.sum(distance['min_distance_squared'])
-            medoid = ids[random_state.choice(range(ids.size), 1, p=distance_values)]
+            distance_values = distance['min_distance_squared'] / np.sum(
+                distance['min_distance_squared'])
+            medoid = ids[random_state.choice(range(ids.size),
+                                             1,
+                                             p=distance_values)]
             medoids.extend(medoid)
 
     medoids = sorted(medoids)
@@ -44,23 +51,31 @@ def _sse(distances, predicted_values, medoids):
     return np.sum(sse)
 
 
-class KMedoids(object):
+class KMedoids(KMeans):
 
-    def __init__(self, n_clusters=8, max_iter=300, n_init=10, batch_size=100, random_state=False):
+    def __init__(self,
+                 n_clusters=8,
+                 max_iter=300,
+                 n_init=10,
+                 batch_size=100,
+                 random_state=False):
         self.n_clusters = n_clusters
         self.max_iter = max_iter
         self.n_init = n_init
         self.random_state = random_state
-
-    def fit_predict(self, X):
-        self.fit(X)
-        return self.labels_
+        self._n_threads = 1
 
     def fit(self, X):
+        """
+        Args:
+            X: array-like or sparse matrix, shape=(n_samples, n_features)
+        Returns:
+            KMedoids
+        """
         random_state = check_random_state(self.random_state)
 
-        X = squareform(pdist(X, metric='euclidean'))
-        m, n = X.shape
+        new_X = squareform(pdist(X, metric='euclidean'))
+        m, n = new_X.shape
 
         col_names = ['x_' + str(i + 1) for i in range(n)]
 
@@ -69,47 +84,56 @@ class KMedoids(object):
         best_medoids = []
         for init_num in range(self.n_init):
             initial_medoids = random_state.choice(range(m), n, replace=False)
-            tmp_X = X[initial_medoids, :]
+            tmp_X = new_X[initial_medoids, :]
 
             labels = np.argmin(tmp_X, axis=1)
 
             results = pd.DataFrame([range(m), labels]).T
             results.columns = ['id', 'label']
 
-            results = pd.concat([results, pd.DataFrame(X, columns=col_names)], axis=1)
+            results = pd.concat(
+                [results, pd.DataFrame(new_X, columns=col_names)], axis=1)
 
             previous_medoids = initial_medoids
             new_medoids = []
 
             loop = 0
-            while len(set(previous_medoids).intersection(set(new_medoids))) != self.n_clusters and loop < self.max_iter:
+            while len(set(previous_medoids).intersection(set(
+                    new_medoids))) != self.n_clusters and loop < self.max_iter:
 
                 if loop > 0:
                     previous_medoids = new_medoids.copy()
                     new_medoids = []
 
                 for i in range(self.n_clusters):
-                    tmp = results.ix[results['label'] == i, :].copy()
+                    tmp = results.loc[results['label'] == i, :].copy()
 
-                    tmp['distance'] = np.sum(tmp.loc[:, col_names].values, axis=1)
+                    tmp['distance'] = np.sum(tmp.loc[:, col_names].values,
+                                             axis=1)
                     tmp.reset_index(drop=True, inplace=True)
                     new_medoids.append(tmp.loc[tmp['distance'].idxmin(), 'id'])
 
                 new_medoids = sorted(new_medoids)
-                tmp_X = X[:, new_medoids]
+                tmp_X = new_X[:, new_medoids]
 
                 results['label'] = np.argmin(tmp_X, axis=1)
 
                 loop += 1
 
-            results = results.ix[:, ['id', 'label']]
+            results = results.loc[:, ['id', 'label']]
             results['flag_medoid'] = 0
             for medoid in new_medoids:
-                results.ix[results['id'] == medoid, 'flag_medoid'] = 1
-            tmp_X = pd.DataFrame(tmp_X, columns=['medoid_distance' + str(i) for i in range(self.n_clusters)])
+                results.loc[results['id'] == medoid, 'flag_medoid'] = 1
+            tmp_X = pd.DataFrame(tmp_X,
+                                 columns=[
+                                     'medoid_distance' + str(i)
+                                     for i in range(self.n_clusters)
+                                 ])
             results = pd.concat([results, tmp_X], axis=1)
 
-            sse = _sse(distances=X, predicted_values=results['label'].values, medoids=new_medoids)
+            sse = _sse(distances=new_X,
+                       predicted_values=results['label'].values,
+                       medoids=new_medoids)
 
             if sse < best_sse:
                 best_sse = sse
@@ -117,7 +141,25 @@ class KMedoids(object):
                 best_medoids = new_medoids.copy()
 
         self.labels_ = best_results['label'].values
-        self.cluster_centers_ = np.array(best_medoids)
+        self.cluster_centers_ = X[best_medoids]
         self.inertia_ = best_sse
 
         return self
+
+    def fit_predict(self, X):
+        """
+        Args:
+            X: array-like or sparse matrix, shape=(n_samples, n_features)
+        Returns:
+            labels: ndarray of shape (n_samples,)
+        """
+        return self.fit(X).labels_
+
+    def fit_transform(self, X):
+        """
+        Args:
+            X: array-like or sparse matrix, shape=(n_samples, n_features)
+        Returns:
+            X_new: ndarray of shape (n_samples, n_clusters)
+        """
+        return self.fit(X)._transform(X)
