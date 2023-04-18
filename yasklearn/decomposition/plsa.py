092a0b1141a7b444377b3408efb5b89a86073923
Yukino Ikegami <yknikgm@gmail.com>
2023-04-19 01:42:10 +0900

    Refactor PLSA

--- yasklearn/decomposition/plsa.py
+++ yasklearn/decomposition/plsa.py
@@ -1,67 +1,76 @@
 import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils import check_random_state
 
 
-class PLSA(object):
-    def __init__(self, n_components=10, max_iter=10, t=1.0e-7, random_state=None):
+class PLSA(BaseEstimator, TransformerMixin):
+    """PLSA algorithm.
+
+  Args:
+    K: The number of latent topics.
+    max_iter: The maximum number of iterations.
+    tol: The tolerance for convergence.
+
+  Returns:
+    A tuple of (theta, phi), where theta is the topic distribution for each document and phi is the word distribution for each topic.
+  """
+
+    def __init__(self,
+                 n_components=10,
+                 max_iter=10,
+                 tol=1.0e-7,
+                 random_state=None):
         self.n_components = n_components
         self.max_iter = max_iter
-        self.t = t
+        self.tol = tol
         self.random_state = random_state
-
-    def _initialize(self, X):
-        random_state = check_random_state(self.random_state)
-
-        # P(z)
-        self.Pz = random_state.rand(self.n_components)
-        # P(x|z)
-        self.Px_z = random_state.rand(self.n_components, X.shape[0])
-        # P(y|z)
-        self.Py_z = random_state.rand(self.n_components, X.shape[1])
-
-        self.Pz /= np.sum(self.Pz)
-        self.Px_z /= np.sum(self.Px_z, axis=1)[:, np.newaxis]
-        self.Py_z /= np.sum(self.Py_z, axis=1)[:, np.newaxis]
-
-    def _em_algorithm(self, X):
-        tmp = X / np.einsum('k,ki,kj->ij', self.Pz, self.Px_z, self.Py_z)
-        tmp[np.isnan(tmp)] = 0
-        tmp[np.isinf(tmp)] = 0
-
-        Pz = np.einsum('ij,k,ki,kj->k', tmp, self.Pz, self.Px_z, self.Py_z)
-        Px_z = np.einsum('ij,k,ki,kj->ki', tmp, self.Pz, self.Px_z, self.Py_z)
-        Py_z = np.einsum('ij,k,ki,kj->kj', tmp, self.Pz, self.Px_z, self.Py_z)
-
-        self.Pz = Pz / np.sum(Pz)
-        self.Px_z = Px_z / np.sum(Px_z, axis=1)[:, np.newaxis]
-        self.Py_z = Py_z / np.sum(Py_z, axis=1)[:, np.newaxis]
-
-    def _log_likelihood(self, X):
-        Pxy = np.einsum('k,ki,kj->ij', self.Pz, self.Px_z, self.Py_z)
-        Pxy /= np.sum(Pxy)
-        lPxy = np.log(Pxy)
-        lPxy[np.isinf(lPxy)] = -1000
-        return np.sum(X * lPxy)
+        check_random_state(self.random_state)
 
     def fit(self, X):
-        self._initialize(X)
+        """Fit the model to the data.
 
-        prev_log_likelihood = 100000
+    Args:
+      X: A NumPy array of documents, where each document is a row.
+
+    Returns:
+      The fitted model.
+    """
+
+        # Initialize the parameters.
+        D = X.shape[0]
+        V = X.shape[1]
+        self.theta = np.random.rand(D, self.n_components)
+        self.phi = np.random.rand(self.n_components, V)
+
+        # Iterate until convergence.
         for i in range(self.max_iter):
-            self._em_algorithm(X)
-            log_likelihood = self._log_likelihood(X)
-            if abs((log_likelihood - prev_log_likelihood) / prev_log_likelihood) < self.t:
+            # E-step: Compute the expected topic distribution for each word.
+            e_theta = np.dot(X, self.phi) / np.sum(
+                self.phi, axis=1, keepdims=True)
+
+            # M-step: Update the topic distribution and word distribution.
+            self.theta = np.dot(X.T, e_theta) / np.sum(
+                e_theta, axis=0, keepdims=True)
+            self.phi = np.dot(e_theta.T, X) / np.sum(
+                e_theta, axis=1, keepdims=True)
+
+            # Check for convergence.
+            diff = np.linalg.norm(self.theta - self.theta_old)
+            if diff < self.tol:
                 break
-            prev_log_likelihood = log_likelihood
+
+            self.theta_old = self.theta
 
         return self
 
     def transform(self, X):
-        Pz = np.einsum('ij,k,ki,kj->k', X, self.Pz, self.Px_z, self.Py_z)
-        Px_z = np.einsum('ij,k,ki,kj->ki', X, self.Pz, self.Px_z, self.Py_z)
-        Pz_x = Px_z.T * Pz[np.newaxis, :]
-        return (Pz_x / np.sum(Pz_x, axis=1)[:, np.newaxis])
+        """Transform the data.
 
-    def fit_transform(self, X):
-        self.fit(X)
-        return self.transform(X)
+    Args:
+      X: A NumPy array of documents, where each document is a row.
+
+    Returns:
+      A NumPy array of topic distributions, where each row corresponds to a document.
+    """
+
+        return np.dot(X, self.phi)
